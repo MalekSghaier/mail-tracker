18886458d9695f810bb4a2bf80082c496afe7166
MalekSghaier <melak.seghaier@gmail.com>
2026-07-03 17:10:41 +0200

    commit

--- agent/obj/Debug/net8.0-windows/maildetectoragent.AssemblyInfo.cs
+++ agent/obj/Debug/net8.0-windows/maildetectoragent.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MailDetectorAgent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb4b32f93858415ead462a999c8092c8ae67b7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+017ee0982b3bf02c458c53a200483dff55dbd801")]
 [assembly: System.Reflection.AssemblyProductAttribute("MailDetectorAgent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MailDetectorAgent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
